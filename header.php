3e0d4ca1aff13610b7ecf3be97f6a65559ebafc6
cenedypalma <61845849+canadypalma@users.noreply.github.com>
2020-11-18 23:39:01 +0600

    my first site in php

---
+++ header.php
@@ -0,0 +1,53 @@
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <!--<meta name="viewport" content="width=device-width, initial-scale=1">-->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<nav class="navbar navbar-expand-md bg-red navbar-light">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="collapsibleNavbar">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="index.php">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="registration.php">Donor Registration</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="requests.php">send Request</a>
+      </li>
+<li class="nav-item">
+        <a class="nav-link" href="viewrequest.php">Leader board</a>
+      </li>
+<li class="nav-item">
+        <a class="nav-link" href="login.php">log In</a>
+      </li>
+<li class="nav-item">
+        <a class="nav-link" href="search.php">Search</a>
+      </li>
+<li class="nav-item">
+        <a class="nav-link" href="contact.php">Contact Us</a>
+      </li>	
+<li class="nav-item">
+        <a class="nav-link" href="aboutus.php">About</a>
+      </li>	  
+    </ul>
+  </div>  
+</nav>
+<br>
+
+<div class="container">
+  
+
+</body>
+</html>
+
+			
